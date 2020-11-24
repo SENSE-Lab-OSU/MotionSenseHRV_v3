d2467d5d14e2dab64467712a417c4fc515bf50b8
dman82499 <devan.mallory@yahoo.com>
2020-11-24 13:21:16 -0500

    updated project with edge delployment source files

---
+++ software/tutorials/AEHR_model_tutorial/Edge-BLE-build/app_core/CodeTF_BLE_integrated/build_nrf5340dk_nrf5340_cpuapp/hci_rpmsg/modules/libmetal/libmetal/lib/system/zephyr/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/devan/Documents/ncs/modules/hal/libmetal/libmetal/lib/system/zephyr
+# Build directory: /home/devan/Documents/MotionSenseHRV_v3/software/tutorials/AEHR_model_tutorial/Edge-BLE-build/app_core/CodeTF_BLE_integrated/build_nrf5340dk_nrf5340_cpuapp/hci_rpmsg/modules/libmetal/libmetal/lib/system/zephyr
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("cortexm")
---
+++ software/tutorials/AEHR_model_tutorial/Edge-BLE-build/app_core/CodeTF_BLE_integrated/src/common.h
@@ -0,0 +1,12 @@
+
+// printer.h file
+#ifndef COMMON_H_
+#define COMMON_H_
+
+ // common.h code goes here
+extern struct bt_conn *my_connection; // BLE conection instance used to access the  sofr-device controller
+extern uint16_t global_counter;
+#endif
+
+
+
