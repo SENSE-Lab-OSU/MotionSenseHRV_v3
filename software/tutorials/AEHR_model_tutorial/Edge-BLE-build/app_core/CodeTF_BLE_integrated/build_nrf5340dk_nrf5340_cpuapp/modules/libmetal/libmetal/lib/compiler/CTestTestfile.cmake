8f3d01ce9137f1dba1044a9d9f8ef71f55b9e6ac
dman82499 <devan.mallory@yahoo.com>
2020-11-24 13:25:51 -0500

    removed accidental build files

--- software/tutorials/AEHR_model_tutorial/Edge-BLE-build/app_core/CodeTF_BLE_integrated/build_nrf5340dk_nrf5340_cpuapp/modules/libmetal/libmetal/lib/compiler/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/devan/Documents/ncs/modules/hal/libmetal/libmetal/lib/compiler
-# Build directory: /home/devan/Documents/MotionSenseHRV_v3/software/tutorials/AEHR_model_tutorial/Edge-BLE-build/app_core/CodeTF_BLE_integrated/build_nrf5340dk_nrf5340_cpuapp/modules/libmetal/libmetal/lib/compiler
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gcc")
-subdirs("iar")
--- software/tutorials/AEHR_model_tutorial/Edge-BLE-build/app_core/CodeTF_BLE_integrated/build_nrf5340dk_nrf5340_cpuapp/modules/open-amp/open-amp/lib/remoteproc/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/devan/Documents/ncs/modules/lib/open-amp/open-amp/lib/remoteproc
-# Build directory: /home/devan/Documents/MotionSenseHRV_v3/software/tutorials/AEHR_model_tutorial/Edge-BLE-build/app_core/CodeTF_BLE_integrated/build_nrf5340dk_nrf5340_cpuapp/modules/open-amp/open-amp/lib/remoteproc
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
