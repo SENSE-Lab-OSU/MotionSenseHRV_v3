1239955b44960f0f0d3fd7df4ed09f876c711c70
dman82499 <devan.mallory@yahoo.com>
2020-11-25 14:35:27 -0500

    removed accidentally added build-specific repositories from remote

--- software/tutorials/AEHR_model_tutorial/Edge-BLE-build/app_core/CodeTF_BLE_integrated4/build_nrf5340pdk_nrf5340_cpuapp/hci_rpmsg/modules/open-amp/open-amp/lib/rpmsg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/devan/Documents/ncs/modules/lib/open-amp/open-amp/lib/rpmsg
-# Build directory: /home/devan/Documents/MotionSenseHRV_v3/software/tutorials/AEHR_model_tutorial/Edge-BLE-build/app_core/CodeTF_BLE_integrated4/build_nrf5340pdk_nrf5340_cpuapp/hci_rpmsg/modules/open-amp/open-amp/lib/rpmsg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- software/tutorials/AEHR_model_tutorial/Edge-BLE-build/app_core/CodeTF_BLE_integrated4/build_nrf5340pdk_nrf5340_cpuapp/modules/libmetal/libmetal/lib/compiler/iar/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/devan/Documents/ncs/modules/hal/libmetal/libmetal/lib/compiler/iar
-# Build directory: /home/devan/Documents/MotionSenseHRV_v3/software/tutorials/AEHR_model_tutorial/Edge-BLE-build/app_core/CodeTF_BLE_integrated4/build_nrf5340pdk_nrf5340_cpuapp/modules/libmetal/libmetal/lib/compiler/iar
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
