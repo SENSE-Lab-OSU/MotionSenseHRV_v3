a6c164fd32ef7e28c26d279fa3e9f8b4d4529dd7
dman82499 <devan.mallory@yahoo.com>
2020-11-30 02:57:27 -0500

    deleted unessesary build files

--- software/tutorials/AEHR_model_tutorial/Edge-BLE-build/net_core/hci_rpmsg/build_nrf5340pdk_nrf5340_cpunet/modules/libmetal/libmetal/lib/processor/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/devan/Documents/ncs/modules/hal/libmetal/libmetal/lib/processor
-# Build directory: /home/devan/Documents/ncs/zephyr/samples/bluetooth/hci_rpmsg/build_nrf5340pdk_nrf5340_cpunet/modules/libmetal/libmetal/lib/processor
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("arm")
--- software/tutorials/AEHR_model_tutorial/Edge-BLE-build/net_core/hci_rpmsg/build_nrf5340pdk_nrf5340_cpunet/modules/open-amp/open-amp/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/devan/Documents/ncs/modules/lib/open-amp/open-amp
-# Build directory: /home/devan/Documents/ncs/zephyr/samples/bluetooth/hci_rpmsg/build_nrf5340pdk_nrf5340_cpunet/modules/open-amp/open-amp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("lib")
